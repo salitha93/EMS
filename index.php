b1e573d12acb62ab07fdd2c239024698d9e96e23
Salitha Nanayakkara <salitha12@gmail.com>
2020-02-23 01:38:14 +0530

    Modifed1

--- index.php
+++ index.php
@@ -1,55 +1,52 @@
-<?php session_start(); ?>
-<?php include('dbcon.php'); ?>
+<?php 
+	include('functions.php');
+
+	if (!isLoggedIn()) {
+		$_SESSION['msg'] = "You must log in first";
+		header('location: login.php');
+	}
+?>
+<!DOCTYPE html>
 <html>
 <head>
-<link rel="stylesheet" type="text/css" href="style.css">
+	<title>Home</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
-<div class="form-wrapper">
-  
-  <form action="#" method="post">
-    <h3>Login here</h3>
-	
-    <div class="form-item">
-		<input type="text" name="user" required="required" placeholder="Username" autofocus required></input>
-    </div>
-    
-    <div class="form-item">
-		<input type="password" name="pass" required="required" placeholder="Password" required></input>
-    </div>
-    
-    <div class="button-panel">
-		<input type="submit" class="button" title="Log In" name="login" value="Login"></input>
-    </div>
-  </form>
-  <?php
-	if (isset($_POST['login']))
-		{
-			$username = mysqli_real_escape_string($con, $_POST['user']);
-			$password = mysqli_real_escape_string($con, $_POST['pass']);
-			
-			$query 		= mysqli_query($con, "SELECT * FROM users WHERE  password='$password' and username='$username'");
-			$row		= mysqli_fetch_array($query);
-			$num_row 	= mysqli_num_rows($query);
-			
-			if ($num_row > 0) 
-				{			
-					$_SESSION['user_id']=$row['user_id'];
-					header('location:home.php');
-					
-				}
-			else
-				{
-					echo 'Invalid Username and Password Combination';
-				}
-		}
-  ?>
-  <div class="reminder">
-    <p>Not a member? <a href="#">Sign up now</a></p>
-    <p><a href="#">Forgot password?</a></p>
-  </div>
-  
-</div>
+	<div class="header">
+		<h2>Home Page</h2>
+	</div>
+	<div class="content">
+		<!-- notification message -->
+		<?php if (isset($_SESSION['success'])) : ?>
+			<div class="error success" >
+				<h3>
+					<?php 
+						echo $_SESSION['success']; 
+						unset($_SESSION['success']);
+					?>
+				</h3>
+			</div>
+		<?php endif ?>
+		<!-- logged in user information -->
+		<div class="profile_info">
+			<img src="images/user_profile.png"  >
 
+			<div>
+				<?php  if (isset($_SESSION['user'])) : ?>
+					<strong><?php echo $_SESSION['user']['username']; ?></strong>
+
+					<small>
+						<i  style="color: #888;">(<?php echo ucfirst($_SESSION['user']['user_type']); ?>)</i> 
+						<br>
+						<a href="paysheet.php?paysheet='1'" style="color: red;">pay-sheet</a>
+						<br>
+						<a href="index.php?logout='1'" style="color: red;">logout</a>
+					</small>
+
+				<?php endif ?>
+			</div>
+		</div>
+	</div>
 </body>
 </html>
