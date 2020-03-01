eeaf9ab7ad7902d377212a173f5e5303556ebe6a
Salitha Nanayakkara <salitha12@gmail.com>
2020-03-01 10:37:28 +0530

    modified

--- db.php
+++ db.php
@@ -1,5 +1,7 @@
 <?php
 
+	session_start();
+
 	$dbhost = "localhost";
 	$dbuser = "admin";
 	$dbpass = "admin4321";
--- pay_sheet.php
+++ pay_sheet.php
@@ -5,6 +5,23 @@
 		$_SESSION['msg'] = "You must log in first";
 		header('location: login.php');
 	}*/
+	$employee_id  		 =  $_SESSION['user']['emp_id'];
+	$employee_name  	 =  "";
+	$month  			 =  "";
+	$basic_salary   	 =  0;
+	$over_time_payment   =  0;
+	$allowances			 = 	0;
+	$leave_deduction	 =	0;
+	$loan_deduction	 	 =	0;
+	$total_salary		 =	0;
+	
+
+	$total_earnings  = $basic_salary + $over_time_payment + $allowances;
+	$total_deduction = $leave_deduction + $loan_deduction;
+
+	$total_salary = $total_earnings - $total_deduction;
+
+
 	if (isset($_POST['submit_logout_btn'])) {
 		session_destroy();
 		unset($_SESSION['user']);
@@ -26,39 +43,39 @@
 		<!-- logged in user information -->
 		<div class="abcd">
 			<label>Employee Name</label><br>
-			<input type="text" name="employee-name" >
+			<input type="text" name="employee_name" >
 		</div>
 		<div class="abcd">
 			<label>Employee ID</label><br>
-			<input type="text" name="employee-id">
+			<input type="text" name="employee_id" value="<?php echo $employee_id ?>">
 		</div>
 		<div class="abcd">
 			<label>Month</label><br>
-			<input type="text" name="month">
+			<input type="text" name="month"  value="<?php echo $month ?>">
 		</div>
 		<div class="abcd">
 			<label>Basic Salary(Rs)</label><br>
-			<input type="text" name="basic-salary">
+			<input type="text" name="basic_salary"  value="<?php echo $basic_salary ?>">
 		</div>
 		<div class="abcd">
 			<label>Overtime Payment(Rs)</label><br>
-			<input type="text" name="over-time-hours">
+			<input type="text" name="over_time_payment" value="<?php echo $over_time_payment ?>">
 		</div>
 		<div class="abcd">
 			<label>Allowances(Rs)</label><br>
-			<input type="text" name="allowance">
+			<input type="text" name="allowances" value="<?php echo $allowances ?>">
 		</div>
 		<div class="abcd">
 			<label>Leave Deduction(Rs)</label><br>
-			<input type="text" name="leave-deduction">
+			<input type="text" name="leave_deduction" value="<?php echo $leave_deduction ?>">
 		</div>
 		<div class="abcd">
 			<label>Loan Deduction(Rs)</label><br>
-			<input type="text" name="loan-deduction">
+			<input type="text" name="loan_deduction" value="<?php echo $loan_deduction ?>">
 		</div>
 		<div class="abcd">
 			<label>Total Salary(Rs)</label><br>
-			<input type="text" name="total-salary">
+			<input type="text" name="total_salary" value="<?php echo $total_salary ?>">
 		</div>
 		<div class="abcd">
 			<button type="submit" class="btn" name="submit_logout_btn">Logout</button>
